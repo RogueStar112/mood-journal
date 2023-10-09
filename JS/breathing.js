4c2e94e104291eb4ccd8314bf1716cf257a97ade
Dominic <dom.benatar@gmail.com>
2023-10-09 13:39:57 +0100

    breathing animation

---
+++ JS/breathing.js
@@ -0,0 +1,7 @@
+// const input = document.querySelector("input[type='number']");
+
+// document
+//   .querySelector("input[type='number']")
+//   .addEventListener("change", (event) => {
+//     document.document.style.setProperty("--timing", input.value + "s");
+//   });
